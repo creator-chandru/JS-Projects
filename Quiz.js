fca99a56a110225b69da04d650f54146e4f07695
creator-chandru <creatorchandru31@gmail.com>
2026-04-29 22:40:23 +0530

    made question objects and started trying to generate html

---
+++ Quiz.js
@@ -0,0 +1,32 @@
+const Questions = [
+    {
+        question: "What Does JS bring to Web Development?",
+        options:['layout','interactivity','style','structure'],
+        answer:'interactivity'
+    },
+    {
+        question: "What is the full form of JSON?",
+        options:['JavaScript Option Net','JavaScript Open Notation','JavaScript Object Notation','JavaScript Object Orientation Notation'],
+        answer:'JavaScript Object Notation'
+    },
+    {
+        question: "Which built-in function is used to execute some action after a delay?",
+        options:['setTimeout()','setInterval()','getTimeout()','clearInterval()'],
+        answer:'setTimeout()'
+    },
+    {
+        question: "Which built-in function in JS helps in sending a HTTP request?",
+        options:['HTTPRequest()','HTMLHttpRequest()','JSHttpRequest()','XMLHttpRequest()'],
+        answer:'XMLHttpRequest()'
+    },
+    {
+        question: "Which of this best describes Effelento?",
+        options:['cool','nerd','crazy','funny'],
+        answer:'crazy'
+    }
+];
+
+const attemptBtn = document.querySelector('.attempt');
+attemptBtn.addEventListener('click',()=>{
+    let questionHTML = '';
+});
